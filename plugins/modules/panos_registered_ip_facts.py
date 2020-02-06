f4826982bfaff24398b7726a463df15981230ef9
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-02-06 14:14:23 -0800

    use collection documentation fragments

--- plugins/modules/panos_registered_ip_facts.py
+++ plugins/modules/panos_registered_ip_facts.py
@@ -36,8 +36,8 @@ requirements:
 notes:
     - Panorama is not supported.
 extends_documentation_fragment:
-    - panos.transitional_provider
-    - panos.vsys
+    - paloaltonetworks.panos.fragments.transitional_provider
+    - paloaltonetworks.panos.fragments.vsys
 options:
     tags:
         description:
