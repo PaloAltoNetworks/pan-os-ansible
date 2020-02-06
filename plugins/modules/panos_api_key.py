9764f3b1e24d6e4e9c112a2ca8249fe6d0dc5120
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-02-06 13:23:58 -0800

    correcting the import statement

--- plugins/modules/panos_api_key.py
+++ plugins/modules/panos_api_key.py
@@ -65,7 +65,7 @@ api_key:
 
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.PaloAltoNetworks.panos.plugins.module_utils.panos import get_connection
+from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import get_connection
 
 
 def main():
