d4edf2b7e20a57aa345edc78112ead28f538887b
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-02-06 13:23:59 -0800

    correcting the import statement

--- plugins/modules/panos_bgp_policy_rule.py
+++ plugins/modules/panos_bgp_policy_rule.py
@@ -225,7 +225,7 @@ RETURN = '''
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.PaloAltoNetworks.panos.plugins.module_utils.panos import get_connection
+from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import get_connection
 from ansible.module_utils._text import to_text
 
 
