42df01d8e9c9cd89d7070de9b77aca6c3e533b3c
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-02-06 13:23:59 -0800

    correcting the import statement

--- plugins/modules/panos_facts.py
+++ plugins/modules/panos_facts.py
@@ -233,7 +233,7 @@ ansible_net_routing_table:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.PaloAltoNetworks.panos.plugins.module_utils.panos import get_connection
+from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import get_connection
 from ansible.module_utils.six import iteritems
 
 
