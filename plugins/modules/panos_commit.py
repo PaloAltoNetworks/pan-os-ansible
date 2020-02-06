d8da2cc75fb84649bf17f75b11246a91e23eaacc
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-02-06 14:14:21 -0800

    use collection documentation fragments

--- plugins/modules/panos_commit.py
+++ plugins/modules/panos_commit.py
@@ -37,8 +37,8 @@ requirements:
     - pan-python can be obtained from PyPI U(https://pypi.python.org/pypi/pan-python)
     - pandevice can be obtained from PyPI U(https://pypi.python.org/pypi/pandevice)
 extends_documentation_fragment:
-    - panos.transitional_provider
-    - panos.device_group
+    - paloaltonetworks.panos.fragments.transitional_provider
+    - paloaltonetworks.panos.fragments.device_group
 options:
     include_template:
         description:
