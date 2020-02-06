c2dbcb68203ce94109ec32bc5021cc18a4e1465d
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-02-06 13:24:00 -0800

    correcting the import statement

--- plugins/modules/panos_nat_rule_facts.py
+++ plugins/modules/panos_nat_rule_facts.py
@@ -158,7 +158,7 @@ listing:
 import re
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.PaloAltoNetworks.panos.plugins.module_utils.panos import get_connection
+from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import get_connection
 
 
 try:
