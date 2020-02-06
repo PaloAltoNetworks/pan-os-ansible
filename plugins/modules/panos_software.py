ce0d92cc615dcff179c37c86041d9ae2e314de69
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-02-06 12:56:23 -0800

    correcting the import statement

--- plugins/modules/panos_software.py
+++ plugins/modules/panos_software.py
@@ -95,7 +95,7 @@ version:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.PaloAltoNetworks.panos.plugins.module_utils.panos import get_connection
+from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import get_connection
 
 try:
     from pandevice import PanOSVersion
