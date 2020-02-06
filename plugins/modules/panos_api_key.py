27e37fe220953526565d063c6fcbc6390781c3e8
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-02-06 14:14:21 -0800

    use collection documentation fragments

--- plugins/modules/panos_api_key.py
+++ plugins/modules/panos_api_key.py
@@ -39,7 +39,7 @@ notes:
     - Panorama is supported.
     - Checkmode is NOT supported.
 extends_documentation_fragment:
-    - panos.transitional_provider
+    - paloaltonetworks.panos.fragments.transitional_provider
 '''
 
 EXAMPLES = '''
