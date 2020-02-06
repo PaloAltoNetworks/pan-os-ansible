d24c982fe52ebe068566c2bc3fcc912811b2b5bb
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-02-06 13:23:59 -0800

    correcting the import statement

--- plugins/modules/panos_interface.py
+++ plugins/modules/panos_interface.py
@@ -198,7 +198,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.basic import get_exception
-from ansible_collections.PaloAltoNetworks.panos.plugins.module_utils.panos import get_connection
+from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import get_connection
 
 
 try:
