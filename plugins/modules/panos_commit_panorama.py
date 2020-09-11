0e40cf4654bb2adcbfa56fb5b1daf1c210d3bfb0
Robert Hagen <rdh@stealthllama.org>
2020-09-11 18:22:56 -0400

    feat: new commit modules

---
+++ plugins/modules/panos_commit_panorama.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2020,  Palo Alto Networks, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: panos_commit_panorama
+short_description: Commit Panorama's candidate configuration.
+description:
+    - Module that will commit the candidate configuration on a Panorama instance.
+    - The new configuration will become active immediately.
+author:
+    - Robert Hagen (@stealthllama)
+version_added: "2.10"
+requirements:
+    - pan-os-python
+    - dateutils
+extends_documentation_fragment:
+    - paloaltonetworks.panos.fragments.provider
+options:
+    description:
+        description:
+            - A description of the commit.
+    admins:
+        description:
+            - Commit only the changes made by specified list of administrators.
+        type: list
+    device_groups:
+        description:
+            - Commit changes made to these device groups.
+        type: list
+    templates:
+        description:
+            - Commit changes made to these templates.
+        type: list
+    template_stacks:
+        description:
+            - Commit changes made to these template stacks.
+        type: list
+    wildfire_appliances:
+        description:
+            - Commit changes made to these WildFire appliances.
+        type: list
+    wildfire_clusters:
+        description:
+            - Commit changes made to these WildFire clusters.
+        type: list
+    log_collectors:
+        description:
+            - Commit changes made to these log collectors.
+        type: list
+    log_collector_groups:
+        description:
+            - Commit changes made to these log collector groups.
+        type: list
+    exclude_device_and_network:
+        description:
+            - Exclude network and device configuration changes.
+        type: bool
+        default: False
+    exclude_shared_objects:
+        description:
+            - Exclude shared object configuration changes.
+        type: bool
+        default: False
+    force:
+        description:
+            - Force the commit.
+        type: bool
+        default: False
+    sync:
+        description:
+            - Wait for the commit to complete.
+        type: bool
+        default: True
+'''
+
+EXAMPLES = r'''
+- name: commit candidate configs on panorama
+  panos_commit_panorama:
+    provider: '{{ credentials }}'
+
+- name: commit changes by specified admins on panorama
+  panos_commit_panorama:
+    provider: '{{ credentials }}'
+    admins: ['netops','secops','cloudops']
+    description: 'Saturday change window'
+
+- name: commit specific device group changes on panorama
+  panos_commit_panorama:
+    provider: '{{ credentials }}'
+    device_groups: ['production','development','testing']
+
+- name: commit log collector group changes on panorama
+  panos_commit_panorama:
+    provider: '{{ credentials }}'
+    log_collector_groups: ['us-west-loggers','apac-loggers','latam-loggers']
+    description: 'Log collector changes'
+'''
+
+RETURN = r'''
+jobid:
+  description: The ID of the PAN-OS commit job.
+  type: int
+  returned: always
+  sample: 49152
+starttime:
+  description: The time the commit job started.
+  type: str
+  returned: on success
+  sample: 2020-09-06T17:56:20
+endtime:
+  description: The time the commit job ended.
+  type: str
+  returned: on success
+  sample: 2020-09-06T17:56:35
+runtime:
+  description: The elapsed time (in seconds) of the commit job.
+  type: int
+  returned: on success
+  sample: 15
+details:
+  description: Commit job completion messages.
+  type: str
+  returned: on success
+  sample: Configuration committed successfully
+warning:
+  description: Commit job warnings.
+  type: list
+  returned: on success
+  sample: ["","","","","","","","","","",""]
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import get_connection
+
+try:
+    from panos.panorama import PanoramaCommit
+    from dateutil import parser
+except ImportError:
+    pass
+
+
+def main():
+    helper = get_connection(
+        min_pandevice_version=(1, 0, 0),
+        min_panos_version=(8, 0, 0),
+        argument_spec=dict(
+            description=dict(type='str'),
+            admins=dict(type='list'),
+            device_groups=dict(type='list'),
+            templates=dict(type='list'),
+            template_stacks=dict(type='list'),
+            wildfire_appliances=dict(type='list'),
+            wildfire_clusters=dict(type='list'),
+            log_collectors=dict(type='list'),
+            log_collector_groups=dict(type='list'),
+            exclude_device_and_network=dict(type='bool'),
+            exclude_shared_objects=dict(type='bool'),
+            force=dict(type='bool'),
+            sync=dict(type='bool', default=True)
+        )
+    )
+
+    module = AnsibleModule(
+        argument_spec=helper.argument_spec,
+        supports_check_mode=False,
+        required_one_of=helper.required_one_of
+    )
+
+    # Verify libs are present, get the parent object.
+    parent = helper.get_pandevice_parent(module)
+
+    # Construct the commit command
+    cmd = PanoramaCommit(
+        description=module.params['description'],
+        admins=module.params['admins'],
+        device_groups=module.params['device_groups'],
+        templates=module.params['templates'],
+        template_stacks=module.params['template_stacks'],
+        wildfire_appliances=module.params['wildfire_appliances'],
+        wildfire_clusters=module.params['wildfire_clusters'],
+        log_collectors=module.params['log_collectors'],
+        log_collector_groups=module.params['log_collector_groups'],
+        exclude_device_and_network=module.params['exclude_device_and_network'],
+        exclude_shared_objects=module.params['exclude_shared_objects'],
+        force=module.params['force']
+    )
+
+    # Execute the commit
+    commit_results = dict(changed=False, jobid=0)
+    sync = module.params['sync']
+    result = parent.commit(cmd=cmd, sync=sync)
+
+    # Exit with status
+    if result is None:
+        # No commit was needed
+        pass
+    elif not sync:
+        # When sync is False only jobid is returned
+        commit_results['jobid'] = int(result)
+    elif not result['success']:
+        # The commit failed
+        module.fail_json(msg=' | '.join(result["messages"]))
+    else:
+        # The commit succeeded
+        commit_results['changed'] = True
+        commit_results['jobid'] = result['jobid']
+        commit_results['starttime'] = parser.parse(result['starttime'])
+        commit_results['endtime'] = parser.parse(result['endtime'])
+        commit_results['runtime'] = (commit_results['endtime'] - commit_results['starttime']).seconds
+        commit_results['warning'] = result['warnings']['line']
+        commit_results['details'] = result['messages']
+
+    module.exit_json(**commit_results)
+
+
+if __name__ == '__main__':
+    main()
---
+++ plugins/modules/panos_commit_push.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2020,  Palo Alto Networks, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: panos_commit_push
+short_description: Commit Panorama's candidate configuration.
+description:
+    - Module that will push the running Panorama configuration to managed devices.
+    - The new configuration will become active immediately.
+author:
+    - Robert Hagen (@stealthllama)
+version_added: "2.10"
+requirements:
+    - pan-os-python
+extends_documentation_fragment:
+    - paloaltonetworks.panos.fragments.provider
+options:
+    style:
+        description:
+            - The type of configuration element to push.
+        type: list
+        elements:
+            - device group
+            - template
+            - template stack
+            - log collector group
+            - wildfire appliance
+            - wildfire cluster
+        required: True
+    name:
+        description:
+            - The name of the configuration element to push.
+    description:
+        description:
+            - A description of the commit.
+    include_template:
+        description:
+            - Include device group reference templates.
+        type: bool
+        default: False
+    force_template_values:
+        description:
+            - Force template values to override local settings.
+        type: bool
+        default: False
+    devices:
+        description:
+            - Push the configuration to specific device serial numbers.
+        type: list
+    sync:
+        description:
+            - Wait for the commit to complete.
+        type: bool
+        default: True
+'''
+
+EXAMPLES = '''
+- name: push device group configs
+  panos_commit_push:
+    provider: '{{ credentials }}'
+    style: 'device group'
+    name: 'Internet Edge Firewalls'
+    description: 'Update ECMP routing'
+
+- name: push template configs and force values
+  panos_commit_push:
+    provider: '{{ credentials }}'
+    style: 'template'
+    name: 'APAC Regional Template'
+    force_template_values: True
+
+- name: push log collector group configs
+  panos_commit_push:
+    provider: '{{ credentials }}'
+    style: 'log collector group'
+    name: 'LatAm Collector Group'
+
+- name: push to multiple devices
+  panos_commit_push:
+    provider: '{{ credentials }}'
+    style: 'device group'
+    name: 'Partner DMZ Firewalls'
+    devices:
+      - 0001234567890
+      - 0987654321000
+      - 1001001F0F000
+
+- name: push to multiple device groups
+  panos_commit_push:
+    provider: '{{ credentials }}'
+    style: 'device group'
+    name: '{{ item }}'
+    sync: False
+  loop:
+    - Production Firewalls
+    - Staging Firewalls
+    - Development Firewalls
+'''
+
+RETURN = '''
+jobid:
+  description: The ID of the PAN-OS commit job.
+  type: int
+  returned: always
+  sample: 49152
+warning:
+  description: Commit job warnings.
+  type: list
+  returned: on success
+  sample: ["","","","","","","","","","",""]
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import get_connection
+
+try:
+    from panos.panorama import PanoramaCommitAll
+    from dateutil import parser
+    from xml.etree import ElementTree as ET
+except ImportError:
+    pass
+
+
+def main():
+    helper = get_connection(
+        min_pandevice_version=(1, 0, 0),
+        min_panos_version=(8, 0, 0),
+        argument_spec=dict(
+            style=dict(type='str'),
+            name=dict(type='str'),
+            description=dict(type='str'),
+            include_template=dict(type='bool'),
+            force_template_values=dict(type='bool'),
+            devices=dict(type='list'),
+            sync=dict(type='bool', default=True)
+        )
+    )
+
+    module = AnsibleModule(
+        argument_spec=helper.argument_spec,
+        supports_check_mode=False,
+        required_one_of=helper.required_one_of
+    )
+
+    # Verify libs are present, get the parent object.
+    parent = helper.get_pandevice_parent(module)
+
+    # Construct the commit command
+    cmd = PanoramaCommitAll(
+        style=module.params['style'],
+        name=module.params['name'],
+        description=module.params['description'],
+        include_template=module.params['include_template'],
+        force_template_values=module.params['force_template_values'],
+        devices=module.params['devices'],
+    )
+
+    # Execute the commit
+    commit_results = {}
+    sync = module.params['sync']
+    result = parent.commit(cmd=cmd, sync=sync, sync_all=sync)
+
+    # Exit with status
+    if not sync:
+        # When sync is False only jobid is returned
+        commit_results['jobid'] = int(result)
+    elif not result['success']:
+        # The commit failed
+        module.fail_json(msg=' | '.join(result['messages']))
+    else:
+        # The commit succeeded
+        commit_results['changed'] = True
+        commit_results['jobid'] = result['jobid']
+        commit_results['warning'] = result['warnings']['line']
+
+    module.exit_json(**commit_results)
+
+
+if __name__ == '__main__':
+    main()
