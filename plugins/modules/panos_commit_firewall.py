e4f420f67d9801923e90cf4a64981d6772e19c40
Michael Richardson <mirichardson@paloaltonetworks.com>
2020-09-18 08:56:18 -0400

    test: Sanity fixes

--- plugins/modules/panos_commit_firewall.py
+++ plugins/modules/panos_commit_firewall.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# The MIT License (MIT)
+#  Copyright 2020 Palo Alto Networks, Inc
 #
-# Copyright (c) 2020,  Palo Alto Networks, Inc.
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
@@ -39,10 +31,9 @@ description:
     - The new configuration will become active immediately.
 author:
     - Robert Hagen (@stealthllama)
-version_added: "2.9"
+version_added: '2.0.0'
 requirements:
     - pan-os-python
-    - dateutils
 extends_documentation_fragment:
     - paloaltonetworks.panos.fragments.provider
 options:
@@ -128,7 +119,7 @@ def main():
         min_panos_version=(8, 0, 0),
         argument_spec=dict(
             description=dict(type='str'),
-            admins=dict(type='list'),
+            admins=dict(type='list', elements='str'),
             exclude_device_and_network=dict(type='bool'),
             exclude_shared_objects=dict(type='bool'),
             exclude_policy_and_objects=dict(type='bool'),
