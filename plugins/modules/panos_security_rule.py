01087bb7709394afd94e8c2f1f63fbbdc36bca0d
Stephen Steiner <ssteiner@paloaltonetworks.com>
2021-06-02 13:48:09 -0400

    Adding rule audit comment - #228
    Signed-off-by: Stephen Steiner <ssteiner@paloaltonetworks.com>

--- plugins/modules/panos_security_rule.py
+++ plugins/modules/panos_security_rule.py
@@ -509,7 +509,8 @@ def main():
 
     # Add the audit comment, if applicable.
     if audit_comment:
-        RuleAuditComment.update(module, audit_comment)
+        rule_audit_comment = RuleAuditComment()
+        rule_audit_comment.update(audit_comment)
 
     # Move the rule to the correct spot, if applicable.
     if module.params["state"] == "present":
