b6e1a78374f5c6ecca8e84a05abf3256e9b99479
Stephen Steiner <ssteiner@paloaltonetworks.com>
2021-06-07 12:53:51 -0400

    230: Added default parameter
    Signed-off-by: Stephen Steiner <ssteiner@paloaltonetworks.com>

--- plugins/modules/panos_email_server.py
+++ plugins/modules/panos_email_server.py
@@ -128,7 +128,7 @@ def main():
             to_email=dict(),
             also_to_email=dict(),
             email_gateway=dict(),
-            protocol=dict(),
+            protocol=dict(type=str, default="SMTP"),
         ),
     )
     module = AnsibleModule(
