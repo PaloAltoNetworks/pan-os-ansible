87f7428b1e81a8fee6bbbe2130c1f1d2e05eb5b9
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-09-10 21:59:41 -0700

    making imports match what exists in other modules

--- plugins/modules/panos_device_group.py
+++ plugins/modules/panos_device_group.py
@@ -17,11 +17,6 @@
 
 from __future__ import absolute_import, division, print_function
 
-from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import (
-    get_connection,
-)
-
 __metaclass__ = type
 
 DOCUMENTATION = """
@@ -87,6 +82,10 @@ RETURN = """
 # Default return values
 """
 
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import (
+    get_connection,
+)
 
 try:
     from panos.errors import PanDeviceError
