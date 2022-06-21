9723a35c9f09e95a7ce79838d887ed04892ddb61
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-06-21 10:32:37 -0700

    check mode again

--- plugins/modules/panos_zone_facts.py
+++ plugins/modules/panos_zone_facts.py
@@ -121,7 +121,7 @@ def main():
     )
     module = AnsibleModule(
         argument_spec=helper.argument_spec,
-        supports_check_mode=False,
+        supports_check_mode=True,
         required_one_of=helper.required_one_of,
     )
 
