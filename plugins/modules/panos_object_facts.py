fb0a5a797e25647ec2603bb2f226ad10811cba69
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-06-21 10:16:07 -0700

    correcting validation errors with supports_check_mode and facts modules

--- plugins/modules/panos_object_facts.py
+++ plugins/modules/panos_object_facts.py
@@ -252,7 +252,6 @@ def main():
 
     module = AnsibleModule(
         argument_spec=helper.argument_spec,
-        supports_check_mode=False,
         required_one_of=helper.required_one_of,
         mutually_exclusive=[
             name_params,
--- plugins/modules/panos_virtual_router_facts.py
+++ plugins/modules/panos_virtual_router_facts.py
@@ -128,7 +128,6 @@ def main():
     )
     module = AnsibleModule(
         argument_spec=helper.argument_spec,
-        supports_check_mode=False,
         required_one_of=helper.required_one_of,
     )
 
