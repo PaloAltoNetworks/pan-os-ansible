16e943198f24596e310ec724c1e97fc7984af2cd
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-07-26 13:47:34 -0700

    feat(panos_application_filter): Add network resource module state support; corrected type of `category`, `subcategory`, `technology` and `risk` to `list`

--- plugins/modules/panos_application_filter.py
+++ plugins/modules/panos_application_filter.py
@@ -43,7 +43,7 @@ extends_documentation_fragment:
     - paloaltonetworks.panos.fragments.transitional_provider
     - paloaltonetworks.panos.fragments.vsys
     - paloaltonetworks.panos.fragments.device_group
-    - paloaltonetworks.panos.fragments.state
+    - paloaltonetworks.panos.fragments.network_resource_module_state
 options:
     name:
         description:
@@ -53,19 +53,23 @@ options:
     category:
         description:
             - Application category
-        type: str
+        type: list
+        elements: str
     subcategory:
         description:
             - Application subcategory
-        type: str
+        type: list
+        elements: str
     technology:
         description:
             - Application technology
-        type: str
+        type: list
+        elements: str
     risk:
         description:
             - Risk (1-5) of the application
-        type: str
+        type: list
+        elements: str
         choices: ['1', '2', '3', '4', '5']
     evasive:
         description:
@@ -115,10 +119,10 @@ EXAMPLES = """
   panos_application_filter:
     provider: '{{ provider }}'
     name: 'custom-apps'
-    category: 'business-systems'
-    subcategory: 'auth-service'
-    technology: 'client-server'
-    risk: '1'
+    category: ['business-systems']
+    subcategory: ['auth-service']
+    technology: ['client-server']
+    risk: ['1']
 
 - name: Remove custom application
   panos_application_filter:
@@ -137,11 +141,9 @@ from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos impor
 )
 
 try:
-    from panos.errors import PanDeviceError
     from panos.objects import ApplicationFilter
 except ImportError:
     try:
-        from pandevice.errors import PanDeviceError
         from pandevice.objects import ApplicationFilter
     except ImportError:
         pass
@@ -152,13 +154,14 @@ def main():
         vsys=True,
         device_group=True,
         with_classic_provider_spec=True,
-        with_state=True,
-        argument_spec=dict(
+        with_network_resource_module_state=True,
+        sdk_cls=ApplicationFilter,
+        sdk_params=dict(
             name=dict(type="str", required=True),
-            category=dict(type="str"),
-            subcategory=dict(type="str"),
-            technology=dict(type="str"),
-            risk=dict(choices=["1", "2", "3", "4", "5"]),
+            category=dict(type="list", elements="str"),
+            subcategory=dict(type="list", elements="str"),
+            technology=dict(type="list", elements="str"),
+            risk=dict(type="list", elements="str", choices=["1", "2", "3", "4", "5"]),
             evasive=dict(type="bool"),
             excessive_bandwidth_use=dict(type="bool"),
             prone_to_misuse=dict(type="bool"),
@@ -178,36 +181,7 @@ def main():
         supports_check_mode=True,
     )
 
-    parent = helper.get_pandevice_parent(module)
-
-    spec = {
-        "name": module.params["name"],
-        "category": module.params["category"],
-        "subcategory": module.params["subcategory"],
-        "technology": module.params["technology"],
-        "risk": module.params["risk"],
-        "evasive": module.params["evasive"],
-        "excessive_bandwidth_use": module.params["excessive_bandwidth_use"],
-        "prone_to_misuse": module.params["prone_to_misuse"],
-        "is_saas": module.params["is_saas"],
-        "transfers_files": module.params["transfers_files"],
-        "tunnels_other_apps": module.params["tunnels_other_apps"],
-        "used_by_malware": module.params["used_by_malware"],
-        "has_known_vulnerabilities": module.params["has_known_vulnerabilities"],
-        "pervasive": module.params["pervasive"],
-        "tag": module.params["tag"],
-    }
-
-    try:
-        listing = ApplicationFilter.refreshall(parent, add=False)
-    except PanDeviceError as e:
-        module.fail_json(msg="Failed refresh: {0}".format(e))
-
-    obj = ApplicationFilter(**spec)
-    parent.add(obj)
-
-    changed, diff = helper.apply_state(obj, listing, module)
-    module.exit_json(changed=changed, diff=diff)
+    helper.process(module)
 
 
 if __name__ == "__main__":
