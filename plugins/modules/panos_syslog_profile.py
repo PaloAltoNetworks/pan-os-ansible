b50258ee0638801d4b3c9de5fd294e29e555e9ae
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-07-26 14:21:28 -0700

    feat(panos_syslog_profile): Add network resource module states

--- plugins/modules/panos_syslog_profile.py
+++ plugins/modules/panos_syslog_profile.py
@@ -37,7 +37,7 @@ extends_documentation_fragment:
     - paloaltonetworks.panos.fragments.transitional_provider
     - paloaltonetworks.panos.fragments.vsys_shared
     - paloaltonetworks.panos.fragments.device_group
-    - paloaltonetworks.panos.fragments.state
+    - paloaltonetworks.panos.fragments.network_resource_module_state
 options:
     name:
         description:
@@ -138,11 +138,9 @@ from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos impor
 
 try:
     from panos.device import SyslogServerProfile
-    from panos.errors import PanDeviceError
 except ImportError:
     try:
         from pandevice.device import SyslogServerProfile
-        from pandevice.errors import PanDeviceError
     except ImportError:
         pass
 
@@ -151,11 +149,12 @@ def main():
     helper = get_connection(
         vsys_shared=True,
         device_group=True,
-        with_state=True,
+        with_network_resource_module_state=True,
         with_classic_provider_spec=True,
         min_pandevice_version=(0, 11, 1),
         min_panos_version=(7, 1, 0),
-        argument_spec=dict(
+        sdk_cls=SyslogServerProfile,
+        sdk_params=dict(
             name=dict(required=True),
             config=dict(),
             system=dict(),
@@ -175,44 +174,14 @@ def main():
             escape_character=dict(),
         ),
     )
+
     module = AnsibleModule(
         argument_spec=helper.argument_spec,
         supports_check_mode=True,
         required_one_of=helper.required_one_of,
     )
 
-    # Verify imports, build pandevice object tree.
-    parent = helper.get_pandevice_parent(module)
-
-    try:
-        listing = SyslogServerProfile.refreshall(parent)
-    except PanDeviceError as e:
-        module.fail_json(msg="Failed refresh: {0}".format(e))
-
-    spec = {
-        "name": module.params["name"],
-        "config": module.params["config"],
-        "system": module.params["system"],
-        "threat": module.params["threat"],
-        "traffic": module.params["traffic"],
-        "hip_match": module.params["hip_match"],
-        "url": module.params["url"],
-        "data": module.params["data"],
-        "wildfire": module.params["wildfire"],
-        "tunnel": module.params["tunnel"],
-        "user_id": module.params["user_id"],
-        "gtp": module.params["gtp"],
-        "auth": module.params["auth"],
-        "sctp": module.params["sctp"],
-        "iptag": module.params["iptag"],
-        "escaped_characters": module.params["escaped_characters"],
-        "escape_character": module.params["escape_character"],
-    }
-    obj = SyslogServerProfile(**spec)
-    parent.add(obj)
-
-    changed, diff = helper.apply_state(obj, listing, module)
-    module.exit_json(changed=changed, diff=diff, msg="Done")
+    helper.process(module)
 
 
 if __name__ == "__main__":
