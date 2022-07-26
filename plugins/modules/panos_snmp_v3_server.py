083fdebc9974e4b0f67cac2af6a05d76029c3164
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-07-26 14:20:52 -0700

    feat(panos_snmp_v3_server): Add network resource module states

--- plugins/modules/panos_snmp_v3_server.py
+++ plugins/modules/panos_snmp_v3_server.py
@@ -37,7 +37,7 @@ extends_documentation_fragment:
     - paloaltonetworks.panos.fragments.transitional_provider
     - paloaltonetworks.panos.fragments.vsys_shared
     - paloaltonetworks.panos.fragments.device_group
-    - paloaltonetworks.panos.fragments.state
+    - paloaltonetworks.panos.fragments.network_resource_module_state
 options:
     snmp_profile:
         description:
@@ -95,11 +95,9 @@ from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos impor
 
 try:
     from panos.device import SnmpServerProfile, SnmpV3Server
-    from panos.errors import PanDeviceError
 except ImportError:
     try:
         from pandevice.device import SnmpServerProfile, SnmpV3Server
-        from pandevice.errors import PanDeviceError
     except ImportError:
         pass
 
@@ -108,12 +106,13 @@ def main():
     helper = get_connection(
         vsys_shared=True,
         device_group=True,
-        with_state=True,
+        with_network_resource_module_state=True,
         with_classic_provider_spec=True,
         min_pandevice_version=(0, 11, 1),
         min_panos_version=(7, 1, 0),
-        argument_spec=dict(
-            snmp_profile=dict(required=True),
+        parents=((SnmpServerProfile, "snmp_profile"),),
+        sdk_cls=SnmpV3Server,
+        sdk_params=dict(
             name=dict(required=True),
             manager=dict(),
             user=dict(),
@@ -122,37 +121,14 @@ def main():
             priv_password=dict(no_log=True),
         ),
     )
+
     module = AnsibleModule(
         argument_spec=helper.argument_spec,
         supports_check_mode=True,
         required_one_of=helper.required_one_of,
     )
 
-    # Verify imports, build pandevice object tree.
-    parent = helper.get_pandevice_parent(module)
-
-    sp = SnmpServerProfile(module.params["snmp_profile"])
-    parent.add(sp)
-    try:
-        sp.refresh()
-    except PanDeviceError as e:
-        module.fail_json(msg="Failed refresh: {0}".format(e))
-
-    listing = sp.findall(SnmpV3Server)
-
-    spec = {
-        "name": module.params["name"],
-        "manager": module.params["manager"],
-        "user": module.params["user"],
-        "engine_id": module.params["engine_id"],
-        "auth_password": module.params["auth_password"],
-        "priv_password": module.params["priv_password"],
-    }
-    obj = SnmpV3Server(**spec)
-    sp.add(obj)
-
-    changed, diff = helper.apply_state(obj, listing, module)
-    module.exit_json(changed=changed, diff=diff, msg="Done")
+    helper.process(module)
 
 
 if __name__ == "__main__":
