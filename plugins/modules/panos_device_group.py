b74f6c37de64f577b51309f7ed0dc2b91fd7c793
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-07-26 14:03:11 -0700

    feat(panos_device_group): Add network resource module states

--- plugins/modules/panos_device_group.py
+++ plugins/modules/panos_device_group.py
@@ -37,7 +37,7 @@ notes:
     - This is a Panorama only module.
     - Check mode is supported.
 extends_documentation_fragment:
-    - paloaltonetworks.panos.fragments.state
+    - paloaltonetworks.panos.fragments.network_resource_module_state
     - paloaltonetworks.panos.fragments.provider
 options:
     name:
@@ -86,86 +86,71 @@ RETURN = """
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.paloaltonetworks.panos.plugins.module_utils.panos import (
     get_connection,
+    ConnectionHelper,
 )
 
 try:
-    from panos.errors import PanDeviceError, PanObjectMissing
+    from panos.errors import PanDeviceError
     from panos.panorama import DeviceGroup
 except ImportError:
     try:
-        from pandevice.errors import PanDeviceError, PanObjectMissing
+        from pandevice.errors import PanDeviceError
         from pandevice.panorama import DeviceGroup
     except ImportError:
         pass
 
 
+class Helper(ConnectionHelper):
+    def pre_state_handling(self, obj, result, module):
+        # Opstate: get the current device group parent.
+        try:
+            obj.opstate.dg_hierarchy.refresh()
+        except PanDeviceError as e:
+            module.fail_json(msg="Failed dg hierarchy refresh: {0}".format(e))
+
+    def post_state_handling(self, obj, result, module):
+        result.setdefault("diff", {})
+        result["diff"]["before_parent"] = obj.opstate.dg_hierarchy.parent
+
+        if module.params["state"] in ("absent", "deleted"):
+            result["diff"]["after_parent"] = None
+        else:
+            parent = module.params["parent"]
+            result["diff"]["after_parent"] = parent
+            if obj.opstate.dg_hierarchy.parent != parent:
+                result["changed"] = True
+                obj.opstate.dg_hierarchy.parent = parent
+                if not module.check_mode:
+                    try:
+                        obj.opstate.dg_hierarchy.update()
+                    except PanDeviceError as e:
+                        module.fail_json(msg="Failed to set dg parent: {0}".format(e))
+
+
 def main():
     helper = get_connection(
-        with_state=True,
+        helper_cls=Helper,
+        with_network_resource_module_state=True,
         firewall_error="This is a Panorama only module",
         min_pandevice_version=(1, 5, 1),
-        argument_spec=dict(
+        with_update_in_apply_state=True,
+        sdk_cls=DeviceGroup,
+        sdk_params=dict(
             name=dict(required=True),
             tag=dict(type="list", elements="str"),
+        ),
+        extra_params=dict(
             parent=dict(),
         ),
     )
+
     module = AnsibleModule(
         argument_spec=helper.argument_spec,
         supports_check_mode=True,
         required_one_of=helper.required_one_of,
     )
 
-    # Verify imports, build pandevice object tree.
-    parent = helper.get_pandevice_parent(module)
-
-    # Object params.
-    spec = {
-        "name": module.params["name"],
-        "tag": module.params["tag"],
-    }
-
-    # Check for current object.
-    live_obj = DeviceGroup(spec["name"])
-    parent.add(live_obj)
-    try:
-        live_obj.refresh()
-    except PanObjectMissing:
-        live_obj = None
-    except PanDeviceError as e:
-        module.fail_json(msg="Failed refresh: {0}".format(e))
-
-    # Build the object and attach to the parent.
-    obj = DeviceGroup(**spec)
-    parent.add(obj)
-
-    # Opstate: get the current device group parent.
-    try:
-        obj.opstate.dg_hierarchy.refresh()
-    except PanDeviceError as e:
-        module.fail_json(msg="Failed dg hierarchy refresh: {0}".format(e))
-
-    # Perform the requeseted action.
-    changed, diff = helper.apply_state_using_update(obj, live_obj, module)
-
-    # Opstate handling: device group parent.
-    diff["before_parent"] = obj.opstate.dg_hierarchy.parent
-    parent = module.params["parent"] or None
-    if module.params["state"] == "absent":
-        diff["after_parent"] = None
-    else:
-        diff["after_parent"] = parent
-        if obj.opstate.dg_hierarchy.parent != parent:
-            obj.opstate.dg_hierarchy.parent = parent
-            changed = True
-            if not module.check_mode:
-                try:
-                    obj.opstate.dg_hierarchy.update()
-                except PanDeviceError as e:
-                    module.fail_json(msg="Failed to set dg parent: {0}".format(e))
-
-    # Done!
-    module.exit_json(changed=changed, diff=diff, msg="Done!")
+    helper.process(module)
 
 
 if __name__ == "__main__":
