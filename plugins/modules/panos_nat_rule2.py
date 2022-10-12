369dbf47b680a95feb8b5cf7ab031a59ae61f976
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-10-11 21:20:50 -0700

    feat(panos_nat_rule2): Add `gathered_filter`

--- plugins/modules/panos_nat_rule2.py
+++ plugins/modules/panos_nat_rule2.py
@@ -42,6 +42,7 @@ notes:
 extends_documentation_fragment:
     - paloaltonetworks.panos.fragments.transitional_provider
     - paloaltonetworks.panos.fragments.network_resource_module_state
+    - paloaltonetworks.panos.fragments.gathered_filter
     - paloaltonetworks.panos.fragments.device_group
     - paloaltonetworks.panos.fragments.vsys
     - paloaltonetworks.panos.fragments.rulebase
@@ -54,7 +55,6 @@ options:
         description:
             - Name of the rule.
         type: str
-        required: true
     description:
         description:
             - The description.
@@ -253,6 +253,7 @@ def main():
         device_group=True,
         rulebase=True,
         with_network_resource_module_state=True,
+        with_gathered_filter=True,
         with_classic_provider_spec=True,
         error_on_firewall_shared=True,
         min_pandevice_version=(1, 7, 3),
