40dc843af7a6306a7fd1afad57a2ed027d49b1d0
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-10-11 21:16:56 -0700

    feat(panos_syslog_profile): Add `gathered_filter`

--- plugins/modules/panos_syslog_profile.py
+++ plugins/modules/panos_syslog_profile.py
@@ -38,12 +38,12 @@ extends_documentation_fragment:
     - paloaltonetworks.panos.fragments.vsys_shared
     - paloaltonetworks.panos.fragments.device_group
     - paloaltonetworks.panos.fragments.network_resource_module_state
+    - paloaltonetworks.panos.fragments.gathered_filter
 options:
     name:
         description:
             - Name of the profile.
         type: str
-        required: true
     config:
         description:
             - Custom config log format.
@@ -142,6 +142,7 @@ def main():
         vsys_shared=True,
         device_group=True,
         with_network_resource_module_state=True,
+        with_gathered_filter=True,
         with_classic_provider_spec=True,
         min_pandevice_version=(0, 11, 1),
         min_panos_version=(7, 1, 0),
