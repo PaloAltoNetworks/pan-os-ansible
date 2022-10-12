6e391625eba85544599f8609b39ac977648b5b73
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-10-11 21:23:40 -0700

    feat(panos_l3_subinterface): Add `gathered_filter`

--- plugins/modules/panos_l3_subinterface.py
+++ plugins/modules/panos_l3_subinterface.py
@@ -40,6 +40,7 @@ notes:
 extends_documentation_fragment:
     - paloaltonetworks.panos.fragments.transitional_provider
     - paloaltonetworks.panos.fragments.network_resource_module_state
+    - paloaltonetworks.panos.fragments.gathered_filter
     - paloaltonetworks.panos.fragments.vsys_import
     - paloaltonetworks.panos.fragments.template_only
 options:
@@ -47,12 +48,10 @@ options:
         description:
             - Name of the interface to configure.
         type: str
-        required: true
     tag:
         description:
             - Tag (vlan id) for the interface
         type: int
-        required: true
     ip:
         description:
             - List of static IP addresses.
@@ -191,6 +190,7 @@ def main():
         template=True,
         with_classic_provider_spec=True,
         with_network_resource_module_state=True,
+        with_gathered_filter=True,
         min_pandevice_version=(0, 8, 0),
         with_set_vsys_reference=True,
         with_set_zone_reference=True,
