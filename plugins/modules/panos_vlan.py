ec7172e0b323df1e90bc8c736a0361b258022907
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-10-11 21:13:36 -0700

    feat(panos_vlan): Add `gathered_filter`

--- plugins/modules/panos_vlan.py
+++ plugins/modules/panos_vlan.py
@@ -33,6 +33,7 @@ requirements:
 extends_documentation_fragment:
     - paloaltonetworks.panos.fragments.transitional_provider
     - paloaltonetworks.panos.fragments.network_resource_module_state
+    - paloaltonetworks.panos.fragments.gathered_filter
     - paloaltonetworks.panos.fragments.vsys_import
     - paloaltonetworks.panos.fragments.full_template_support
 notes:
@@ -43,7 +44,6 @@ options:
         description:
             -  Name of the VLAN.
         type: str
-        required: True
     interface:
         description:
             -  List of interface names
@@ -80,6 +80,7 @@ def main():
         template=True,
         template_stack=True,
         with_network_resource_module_state=True,
+        with_gathered_filter=True,
         with_classic_provider_spec=True,
         with_set_vsys_reference=True,
         sdk_cls=("network", "Vlan"),
