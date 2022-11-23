7a3ec73cc5e1dc078833f86ecf41f27311151d34
James Holland <jamoiholland@gmail.com>
2022-11-23 20:29:59 +0000

    chore: Add AutomationHub publishing alongside Galaxy (#358)

--- .github/do-release.sh
+++ .github/do-release.sh
@@ -2,3 +2,4 @@
 
 ansible-galaxy collection build
 ansible-galaxy collection publish paloaltonetworks-panos-*
+ansible-galaxy collection publish --server https://console.redhat.com/api/automation-hub/content/inbound-paloaltonetworks/ paloaltonetworks-panos-*
