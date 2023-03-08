4ae39b2d509f764c12203204d32d6879a7670ca4
Jimmy Holland <6574404+jamesholland-uk@users.noreply.github.com>
2023-03-08 17:37:51 +0000

    More code formatting

--- plugins/modules/panos_commit_push.py
+++ plugins/modules/panos_commit_push.py
@@ -210,12 +210,18 @@ def main():
         # The commit failed
         fail_message = "Job ID " + result["jobid"] + ": "
 
-        for device in result["devices"].items():  # Iterate over all devices that received the commit_push
+        for device in result[
+            "devices"
+        ].items():  # Iterate over all devices that received the commit_push
             # In the tuples being iterated over here, index 0 is the serial number, index 1 is a dict of commit output messaging
 
-            if not device[1]["success"]:  # For any devices where the commit_push was not successful...
+            if not device[1][
+                "success"
+            ]:  # For any devices where the commit_push was not successful...
                 # Add the name of the device and the commit messages
-                fail_message += device[1]["name"] + ": " + " | ".join(device[1]["messages"]) + "; "
+                fail_message += (
+                    device[1]["name"] + ": " + " | ".join(device[1]["messages"]) + "; "
+                )
 
         module.fail_json(msg=fail_message)
     else:
