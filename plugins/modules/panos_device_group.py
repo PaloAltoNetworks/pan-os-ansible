d17977e7160c6342d5fb5a3fb035fe5234a4e515
Jimmy Holland <6574404+jamesholland-uk@users.noreply.github.com>
2023-09-01 13:36:10 +0100

    fix(panos_device_group): After review, use a more maintainable approach to the logic

--- plugins/modules/panos_device_group.py
+++ plugins/modules/panos_device_group.py
@@ -112,9 +112,10 @@ class Helper(ConnectionHelper):
         else:
             parent = module.params["parent"]
             result["diff"]["after_parent"] = parent
-            if (
-                obj.opstate.dg_hierarchy.parent != parent
-                and module.params["state"] != "gathered"
+            if obj.opstate.dg_hierarchy.parent != parent and module.params["state"] in (
+                "present",
+                "replaced",
+                "merged",
             ):
                 result["changed"] = True
                 obj.opstate.dg_hierarchy.parent = parent
