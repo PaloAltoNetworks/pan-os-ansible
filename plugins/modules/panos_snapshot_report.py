c64cd7902b4e4a83c12a53036052a9c82070af1a
Adrian Celebanski <135693994+acelebanski@users.noreply.github.com>
2024-05-16 12:50:09 +0200

    feat: Add additional error handling to some upgrade assurance modules (#561)

--- plugins/modules/panos_snapshot_report.py
+++ plugins/modules/panos_snapshot_report.py
@@ -166,6 +166,7 @@ PUA_AVAILABLE = True
 try:
     import panos_upgrade_assurance
     from panos_upgrade_assurance.snapshot_compare import SnapshotCompare
+    from panos_upgrade_assurance.exceptions import SnapshotSchemeMismatchException
 except ImportError:
     PUA_AVAILABLE = False
     pass
@@ -198,10 +199,13 @@ def main():
             )
         )
 
-    results = SnapshotCompare(
-        left_snapshot=module.params["left_snapshot"],
-        right_snapshot=module.params["right_snapshot"],
-    ).compare_snapshots(reports=module.params["reports"])
+    try:
+        results = SnapshotCompare(
+            left_snapshot=module.params["left_snapshot"],
+            right_snapshot=module.params["right_snapshot"],
+        ).compare_snapshots(reports=module.params["reports"])
+    except SnapshotSchemeMismatchException as exc:
+        module.fail_json(msg=getattr(exc, "message", repr(exc)))
 
     module.exit_json(changed=False, response=results)
 
